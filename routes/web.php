caffc6dfc844a1248b0052ec02c766f73bf828db
Nur Afina <finanurafina96@gmail.com>
2023-11-04 16:04:40 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/nilaipersepsi', function () {
+    return view('nilaipersepsi');
+});
